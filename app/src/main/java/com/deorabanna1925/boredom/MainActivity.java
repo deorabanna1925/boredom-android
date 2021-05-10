50fcc5a33fbc943012405cc1c8a14fb1845aff96
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2021-05-10 17:26:31 +0530

    first commit

---
+++ app/src/main/java/com/deorabanna1925/boredom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.deorabanna1925.boredom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
