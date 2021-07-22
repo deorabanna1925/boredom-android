863c2d8a97f9f6c7c8c5bca0c1d4a20f1352869f
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2021-07-22 22:03:09 +0530

    Text Speech Added

---
+++ app/src/main/java/com/deorabanna1925/boredom/activity/TextSpeechActivity.java
@@ -0,0 +1,23 @@
+package com.deorabanna1925.boredom.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.deorabanna1925.boredom.databinding.ActivityTextSpeechBinding;
+
+public class TextSpeechActivity extends AppCompatActivity {
+
+    private ActivityTextSpeechBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTextSpeechBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+
+}
