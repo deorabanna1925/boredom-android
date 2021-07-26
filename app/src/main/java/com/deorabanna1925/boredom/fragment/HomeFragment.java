f4e8805eaf14d9739404ec69c262ac352ad7bd80
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2021-07-26 23:24:24 +0530

    DayTime On Home

--- app/src/main/java/com/deorabanna1925/boredom/fragment/HomeFragment.java
+++ app/src/main/java/com/deorabanna1925/boredom/fragment/HomeFragment.java
@@ -1,17 +1,16 @@
 package com.deorabanna1925.boredom.fragment;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.deorabanna1925.boredom.R;
-import com.deorabanna1925.boredom.databinding.ActivityDashboardBinding;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.deorabanna1925.boredom.databinding.FragmentHomeBinding;
+
+import java.util.Calendar;
 
 public class HomeFragment extends Fragment {
 
@@ -25,9 +24,26 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater,container,false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        getDayTime();
 
         return view;
     }
+
+    private void getDayTime() {
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        if(timeOfDay < 12){
+            binding.lottieView.setProgress(0.8f);
+        }else if(timeOfDay < 16){
+            binding.lottieView.setProgress(1.0f);
+        }else if(timeOfDay < 21){
+            binding.lottieView.setProgress(0.2f);
+        }else {
+            binding.lottieView.setProgress(0.5f);
+        }
+    }
+
 }
