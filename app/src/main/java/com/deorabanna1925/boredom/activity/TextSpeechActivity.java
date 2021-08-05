f87a8e158056cf2eaea945ac7030bb680ca7389c
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2021-08-05 21:37:23 +0530

    TextSpeech Added

--- app/src/main/java/com/deorabanna1925/boredom/activity/TextSpeechActivity.java
+++ app/src/main/java/com/deorabanna1925/boredom/activity/TextSpeechActivity.java
@@ -1,23 +1,39 @@
 package com.deorabanna1925.boredom.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.deorabanna1925.boredom.databinding.ActivityTextSpeechBinding;
 
+import java.util.Locale;
+
 public class TextSpeechActivity extends AppCompatActivity {
 
     private ActivityTextSpeechBinding binding;
 
+    @SuppressLint("ObsoleteSdkInt")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTextSpeechBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //Convert Text to Speech
-        //Convert Speech to Text
+        binding.speak.setOnClickListener(view -> {
+            if (binding.text.getText().toString().length() == 0) {
+                binding.text.setError("");
+                return;
+            }
+
+            TextToSpeech tts = new TextToSpeech(this, i -> {
+
+            });
+            tts.setLanguage(Locale.US);
+            tts.speak(binding.text.getText(), TextToSpeech.QUEUE_FLUSH, null, null);
+
+        });
 
     }
 }
