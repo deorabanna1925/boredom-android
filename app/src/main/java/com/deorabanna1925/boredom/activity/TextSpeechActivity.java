6ac9fbf74886343538e45172f31d25b9b54ff8d4
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2021-08-06 22:22:01 +0530

    Text to Speech Done

--- app/src/main/java/com/deorabanna1925/boredom/activity/TextSpeechActivity.java
+++ app/src/main/java/com/deorabanna1925/boredom/activity/TextSpeechActivity.java
@@ -1,8 +1,8 @@
 package com.deorabanna1925.boredom.activity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,27 +13,47 @@ import java.util.Locale;
 public class TextSpeechActivity extends AppCompatActivity {
 
     private ActivityTextSpeechBinding binding;
+    private TextToSpeech textToSpeech;
 
-    @SuppressLint("ObsoleteSdkInt")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTextSpeechBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        
+        getSupportActionBar().setTitle("Text to Speech");
 
-        binding.speak.setOnClickListener(view -> {
-            if (binding.text.getText().toString().length() == 0) {
-                binding.text.setError("");
-                return;
+        textToSpeech = new TextToSpeech(this, status -> {
+            if (status == TextToSpeech.SUCCESS) {
+                int result = textToSpeech.setLanguage(Locale.ENGLISH);
+                if (result == TextToSpeech.LANG_MISSING_DATA
+                        || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(this, "Language Not Supported", Toast.LENGTH_SHORT).show();
+                }
             }
-
-            TextToSpeech tts = new TextToSpeech(this, i -> {
-
-            });
-            tts.setLanguage(Locale.US);
-            tts.speak(binding.text.getText(), TextToSpeech.QUEUE_FLUSH, null, null);
-
         });
 
+        binding.viewAudioPlay.setOnClickListener(view -> speakNow());
+
+    }
+
+    private void speakNow() {
+        float pitch = (float) binding.seekBarPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+        float speed = (float) binding.seekBarSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+
+        textToSpeech.setPitch(pitch);
+        textToSpeech.setSpeechRate(speed);
+        textToSpeech.speak(binding.text.getText().toString(), TextToSpeech.QUEUE_FLUSH, null,null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
     }
 }
