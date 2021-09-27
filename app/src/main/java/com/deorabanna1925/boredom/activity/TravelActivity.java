fe3c023067e201c27f973e99fff2971c5d10fba5
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2021-09-27 22:27:18 +0530

    Changes

---
+++ app/src/main/java/com/deorabanna1925/boredom/activity/TravelActivity.java
@@ -0,0 +1,19 @@
+package com.deorabanna1925.boredom.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.deorabanna1925.boredom.databinding.ActivityTravelBinding;
+
+public class TravelActivity extends AppCompatActivity {
+
+    private ActivityTravelBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTravelBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
