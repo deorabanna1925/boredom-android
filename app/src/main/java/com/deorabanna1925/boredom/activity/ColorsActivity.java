d7df3fe3fba49b15abf4dfd1d83bb5f23623661a
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2022-09-09 21:27:14 +0530

    ColorsActivity Changes

--- app/src/main/java/com/deorabanna1925/boredom/activity/ColorsActivity.java
+++ app/src/main/java/com/deorabanna1925/boredom/activity/ColorsActivity.java
@@ -42,6 +42,8 @@ public class ColorsActivity extends AppCompatActivity {
 
     }
 
+
+
     private void getColorData() {
         binding.progressBar.setVisibility(View.VISIBLE);
         String url = "http://www.colourlovers.com/api/colors/random&format=json";
@@ -84,11 +86,110 @@ public class ColorsActivity extends AppCompatActivity {
 
                 findTextColorOnBackground(hex);
 
+                int[] lightColors = generateColor(hexCode,"#ffffff",20);
+                int[] darkColors = generateColor("#000000",hexCode,19);
+
+                binding.colorVariationLight.removeAllViews();
+                binding.colorVariationDark.removeAllViews();
+                for (int lightColor : lightColors) {
+                    View view = new View(this);
+                    view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
+                    view.setBackgroundColor(Color.parseColor("#" + Integer.toHexString(lightColor)));
+                    binding.colorVariationLight.addView(view);
+                }
+                for (int darkColor : darkColors) {
+                    View view = new View(this);
+                    view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
+                    view.setBackgroundColor(Color.parseColor("#" + Integer.toHexString(darkColor)));
+                    binding.colorVariationDark.addView(view);
+                }
+
+                binding.progressBar.setVisibility(View.GONE);
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }, error -> binding.progressBar.setVisibility(View.GONE));
         queue.add(request);
+    }
+
+    private int[] generateColor(String startColor, String endColor, int count) {
+//        the beginning of your gradient
+        int[] start = getRGB(startColor);
+//        the end of your gradient
+        int[] end = getRGB(endColor);
+//        the number of colors to compute
+        //        alpha blending amount
+        double alpha = 0.0;
+        int[] saida = new int[count];
+        for (int i = 0; i < count; i++) {
+            int[] c = new int[3];
+            alpha += 1.0 / count;
+            c[0] = (int) (start[0] * alpha + (1 - alpha) * end[0]);
+            c[1] = (int) (start[1] * alpha + (1 - alpha) * end[1]);
+            c[2] = (int) (start[2] * alpha + (1 - alpha) * end[2]);
+
+            saida[i] = getHex(c[0], c[1], c[2]);
+        }
+        return saida;
+    }
+
+    private int getHex(int r, int g, int b) {
+        return Color.rgb(r, g, b);
+    }
+
+    private int[] getRGB(String hexColor) {
+        int color = Color.parseColor(hexColor);
+        int r = Color.red(color);
+        int g = Color.green(color);
+        int b = Color.blue(color);
+        return new int[]{r, g, b};
+    }
+
+    private void findMoreColors(int light,String hex,int dark) {
+        hex = "#" + hex;
+//        create 15 shades of color by mixing white
+        String[] lightColors = new String[light];
+        for (int i = 0; i < light; i++) {
+            lightColors[i] = mixColors("#FFFFFF", hex, (float) i / light);
+        }
+//        create 15 shades of color by mixing black
+        String[] darkColors = new String[dark];
+        for (int i = 0; i < dark; i++) {
+            darkColors[i] = mixColors("#000000", hex, (float) i / dark);
+        }
+
+        binding.colorVariationLight.removeAllViews();
+        for (String lightColor : lightColors) {
+            View view = new View(this);
+            view.setBackgroundColor(Color.parseColor(lightColor));
+            view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
+            binding.colorVariationLight.addView(view);
+        }
+        binding.colorVariationDark.removeAllViews();
+        for (String darkColor : darkColors) {
+            View view = new View(this);
+            view.setBackgroundColor(Color.parseColor(darkColor));
+            view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
+            binding.colorVariationDark.addView(view);
+        }
+
+
+
+
+    }
+
+    private String mixColors(String s, String hex, float v) {
+        int color1 = Color.parseColor(s);
+        int color2 = Color.parseColor(hex);
+        float[] hsv1 = new float[3];
+        float[] hsv2 = new float[3];
+        Color.colorToHSV(color1, hsv1);
+        Color.colorToHSV(color2, hsv2);
+        float h = (hsv2[0] - hsv1[0]) * v + hsv1[0];
+        float s1 = (hsv2[1] - hsv1[1]) * v + hsv1[1];
+        float b = (hsv2[2] - hsv1[2]) * v + hsv1[2];
+        return String.format("#%02x%02x%02x", Math.round(Color.red(Color.HSVToColor(new float[]{h, s1, b}))), Math.round(Color.green(Color.HSVToColor(new float[]{h, s1, b}))), Math.round(Color.blue(Color.HSVToColor(new float[]{h, s1, b}))));
     }
 
     private void findTextColorOnBackground(String hex) {
