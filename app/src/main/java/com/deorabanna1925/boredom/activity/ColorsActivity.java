7625358abe488b35b49741bced64bb076d1a26df
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2022-09-10 22:18:37 +0530

    ColorsActivity Changes

--- app/src/main/java/com/deorabanna1925/boredom/activity/ColorsActivity.java
+++ app/src/main/java/com/deorabanna1925/boredom/activity/ColorsActivity.java
@@ -1,11 +1,12 @@
 package com.deorabanna1925.boredom.activity;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
@@ -18,9 +19,12 @@ import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.deorabanna1925.boredom.databinding.ActivityColorsBinding;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -33,87 +37,118 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         ActionBar actionBar = getSupportActionBar();
-        assert actionBar != null;
-        actionBar.hide();
+        if (actionBar != null) actionBar.hide();
 
-        binding.generateNew.setOnClickListener(view -> getColorData());
+        String color = getRandomHexColor();
+        binding.backgroundColor.setBackgroundColor(Color.parseColor(color));
+        binding.hex.setText(color);
+        binding.hex.setTextColor(Color.parseColor(findTextColorOnBackground(color)));
+        int[] rgbCode = convertHexToRgb(color);
+        String rgb = "rgb(" + rgbCode[0] + ", " + rgbCode[1] + ", " + rgbCode[2] + ")";
+        binding.rgb.setText(rgb);
+        binding.rgb.setTextColor(Color.parseColor(findTextColorOnBackground(color)));
+        int[] hsvCode = convertHexToHsv(color);
+        String hsv = "hsv(" + hsvCode[0] + ", " + hsvCode[1] + ", " + hsvCode[2] + ")";
+        binding.hsv.setText(hsv);
+        binding.hsv.setTextColor(Color.parseColor(findTextColorOnBackground(color)));
 
-        getColorData();
+        binding.allShadesTitle.setTextColor(Color.parseColor(color));
+        binding.allShadesCard.setCardBackgroundColor(Color.parseColor(findTextColorOnBackground(color)));
 
+        getImageData(color);
+        String removedHashFromHex = color.replace("#", "");
+        Glide.with(this).load("https://seewhizz.com/noise.php?hex=").into(binding.colorImage);
+
+        String[] lightColors = generateColor(color,"#ffffff",10);
+        String[] darkColors = generateColor("#000000",color,10);
+        ArrayList<String> colors = new ArrayList<>();
+        Collections.addAll(colors, lightColors);
+        colors.add(color);
+        Collections.addAll(colors, darkColors);
+        binding.allShades.removeAllViews();
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0,8,0,8);
+        tvParams.setMargins(16,32,16,32);
+        params.gravity = Gravity.CENTER;
+        for (String hexColor : colors) {
+            LinearLayout view = new LinearLayout(this);
+            TextView textView = new TextView(this);
+            textView.setText(hexColor.toUpperCase(Locale.ENGLISH));
+            textView.setLayoutParams(tvParams);
+            textView.setTextSize(32);
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextColor(Color.parseColor(findTextColorOnBackground(hexColor)));
+            view.addView(textView);
+            view.setLayoutParams(params);
+            view.setBackgroundColor(Color.parseColor(hexColor));
+            binding.allShades.addView(view);
+        }
     }
 
+    private String findTextColorOnBackground(String hex) {
+        int color = Color.parseColor(hex);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        int luminance = (int) Math.sqrt(
+                red * red * .241 +
+                        green * green * .691 +
+                        blue * blue * .068);
+        if (luminance > 130) {
+            return "#000000";
+        } else {
+            return "#ffffff";
+        }
+    }
 
+    private String getRandomHexColor() {
+        StringBuilder hexCode = new StringBuilder("#");
+        for (int i = 0; i < 6; i++) {
+            int random = (int) (Math.random() * 16);
+            hexCode.append(Integer.toHexString(random));
+        }
+        return hexCode.toString();
+    }
 
-    private void getColorData() {
-        binding.progressBar.setVisibility(View.VISIBLE);
-        String url = "http://www.colourlovers.com/api/colors/random&format=json";
+    private int[] convertHexToRgb(String hex) {
+        int red = Integer.parseInt(hex.substring(1, 3), 16);
+        int green = Integer.parseInt(hex.substring(3, 5), 16);
+        int blue = Integer.parseInt(hex.substring(5, 7), 16);
+        return new int[]{red, green, blue};
+    }
+
+    private int[] convertHexToHsv(String hex){
+        int[] rgb = convertHexToRgb(hex);
+        int red = rgb[0];
+        int green = rgb[1];
+        int blue = rgb[2];
+        float[] hsv = new float[3];
+        Color.RGBToHSV(red, green, blue, hsv);
+        int hue = (int) hsv[0];
+        int saturation = (int) (hsv[1] * 100);
+        int value = (int) (hsv[2] * 100);
+        return new int[]{hue,saturation,value};
+    }
+
+    private void getImageData(String hex) {
+        hex = hex.substring(1);
+        String url = "https://php-noise.com/noise.php?hex=" + hex + "&json";
         final RequestQueue queue = Volley.newRequestQueue(this);
         queue.getCache().clear();
         StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
             try {
-                JSONArray jsonArray = new JSONArray(response);
-                JSONObject jsonObject = jsonArray.getJSONObject(0);
-                String hex = jsonObject.getString("hex");
-
-                JSONObject rgb = jsonObject.getJSONObject("rgb");
-                JSONObject hsv = jsonObject.getJSONObject("hsv");
-
-                int r = rgb.getInt("red");
-                int g = rgb.getInt("green");
-                int b = rgb.getInt("blue");
-
-                int h = hsv.getInt("hue");
-                int s = hsv.getInt("saturation");
-                int v = hsv.getInt("value");
-
-                String hexCode = "#" + hex;
-
-                binding.hexValue.setText(hexCode);
-
-                binding.hexValue.setOnClickListener(view -> {
-                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                    ClipData clip = ClipData.newPlainText("hexCode", hexCode);
-                    clipboard.setPrimaryClip(clip);
-                    Toast.makeText(this, "Copy to Clipboard", Toast.LENGTH_SHORT).show();
-                });
-
-                String rgbValue = "RGB(" + r + ", " + g + ", " + b + ")";
-                binding.rgbValue.setText(rgbValue);
-                String hsvValue = "HSV(" + h + ", " + s + ", " + v + ")";
-                binding.hsvValue.setText(hsvValue);
-
-                getImageData(hex);
-
-                findTextColorOnBackground(hex);
-
-                int[] lightColors = generateColor(hexCode,"#ffffff",20);
-                int[] darkColors = generateColor("#000000",hexCode,19);
-
-                binding.colorVariationLight.removeAllViews();
-                binding.colorVariationDark.removeAllViews();
-                for (int lightColor : lightColors) {
-                    View view = new View(this);
-                    view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
-                    view.setBackgroundColor(Color.parseColor("#" + Integer.toHexString(lightColor)));
-                    binding.colorVariationLight.addView(view);
-                }
-                for (int darkColor : darkColors) {
-                    View view = new View(this);
-                    view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
-                    view.setBackgroundColor(Color.parseColor("#" + Integer.toHexString(darkColor)));
-                    binding.colorVariationDark.addView(view);
-                }
-
-                binding.progressBar.setVisibility(View.GONE);
-
+                JSONObject jsonObject = new JSONObject(response);
+                String uri = jsonObject.getString("uri");
+                Glide.with(this).load(Uri.parse(uri)).into(binding.colorImage);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        }, error -> binding.progressBar.setVisibility(View.GONE));
+        }, error -> Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show());
         queue.add(request);
     }
 
-    private int[] generateColor(String startColor, String endColor, int count) {
+    private String[] generateColor(String startColor, String endColor, int count) {
 //        the beginning of your gradient
         int[] start = getRGB(startColor);
 //        the end of your gradient
@@ -128,16 +163,23 @@ public class ColorsActivity extends AppCompatActivity {
             c[0] = (int) (start[0] * alpha + (1 - alpha) * end[0]);
             c[1] = (int) (start[1] * alpha + (1 - alpha) * end[1]);
             c[2] = (int) (start[2] * alpha + (1 - alpha) * end[2]);
-
             saida[i] = getHex(c[0], c[1], c[2]);
         }
-        return saida;
+        return convertIntToHex(saida);
     }
+
+    private String[] convertIntToHex(int[] saida) {
+        String[] hex = new String[saida.length];
+        for (int i = 0; i < saida.length; i++) {
+            hex[i] = String.format("#%06X", (0xFFFFFF & saida[i]));
+        }
+        return hex;
+    }
+
 
     private int getHex(int r, int g, int b) {
         return Color.rgb(r, g, b);
     }
-
     private int[] getRGB(String hexColor) {
         int color = Color.parseColor(hexColor);
         int r = Color.red(color);
@@ -146,88 +188,4 @@ public class ColorsActivity extends AppCompatActivity {
         return new int[]{r, g, b};
     }
 
-    private void findMoreColors(int light,String hex,int dark) {
-        hex = "#" + hex;
-//        create 15 shades of color by mixing white
-        String[] lightColors = new String[light];
-        for (int i = 0; i < light; i++) {
-            lightColors[i] = mixColors("#FFFFFF", hex, (float) i / light);
-        }
-//        create 15 shades of color by mixing black
-        String[] darkColors = new String[dark];
-        for (int i = 0; i < dark; i++) {
-            darkColors[i] = mixColors("#000000", hex, (float) i / dark);
-        }
-
-        binding.colorVariationLight.removeAllViews();
-        for (String lightColor : lightColors) {
-            View view = new View(this);
-            view.setBackgroundColor(Color.parseColor(lightColor));
-            view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
-            binding.colorVariationLight.addView(view);
-        }
-        binding.colorVariationDark.removeAllViews();
-        for (String darkColor : darkColors) {
-            View view = new View(this);
-            view.setBackgroundColor(Color.parseColor(darkColor));
-            view.setLayoutParams(new ActionBar.LayoutParams(100, 100));
-            binding.colorVariationDark.addView(view);
-        }
-
-
-
-
-    }
-
-    private String mixColors(String s, String hex, float v) {
-        int color1 = Color.parseColor(s);
-        int color2 = Color.parseColor(hex);
-        float[] hsv1 = new float[3];
-        float[] hsv2 = new float[3];
-        Color.colorToHSV(color1, hsv1);
-        Color.colorToHSV(color2, hsv2);
-        float h = (hsv2[0] - hsv1[0]) * v + hsv1[0];
-        float s1 = (hsv2[1] - hsv1[1]) * v + hsv1[1];
-        float b = (hsv2[2] - hsv1[2]) * v + hsv1[2];
-        return String.format("#%02x%02x%02x", Math.round(Color.red(Color.HSVToColor(new float[]{h, s1, b}))), Math.round(Color.green(Color.HSVToColor(new float[]{h, s1, b}))), Math.round(Color.blue(Color.HSVToColor(new float[]{h, s1, b}))));
-    }
-
-    private void findTextColorOnBackground(String hex) {
-        int color = Color.parseColor("#" + hex);
-        int red = Color.red(color);
-        int green = Color.green(color);
-        int blue = Color.blue(color);
-        int luminance = (int) Math.sqrt(
-                red * red * .241 +
-                        green * green * .691 +
-                        blue * blue * .068);
-        if (luminance > 130) {
-            binding.hexValue.setTextColor(Color.BLACK);
-            binding.rgbValue.setTextColor(Color.BLACK);
-            binding.hsvValue.setTextColor(Color.BLACK);
-        } else {
-            binding.hexValue.setTextColor(Color.WHITE);
-            binding.rgbValue.setTextColor(Color.WHITE);
-            binding.hsvValue.setTextColor(Color.WHITE);
-        }
-    }
-
-    private void getImageData(String hex) {
-        String url = "https://php-noise.com/noise.php?hex=" + hex + "&json";
-        final RequestQueue queue = Volley.newRequestQueue(this);
-        queue.getCache().clear();
-        StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
-            binding.progressBar.setVisibility(View.GONE);
-            try {
-                JSONObject jsonObject = new JSONObject(response);
-                String uri = jsonObject.getString("uri");
-                Glide.with(this).load(uri).into(binding.imageBackground);
-                String hexCode = "#" + hex;
-                binding.colorImage.setColorFilter(Color.parseColor(hexCode));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }, error -> binding.progressBar.setVisibility(View.GONE));
-        queue.add(request);
-    }
 }
