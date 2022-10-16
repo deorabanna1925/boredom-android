7b8725690ead61a9b82aaba9718f62ca814c6c3f
Pushpendra Singh Deora <royalbannasa365@gmail.com>
2022-10-16 19:56:51 +0530

    changes

--- app/src/main/java/com/deorabanna1925/boredom/activity/InstaViewerActivity.java
+++ app/src/main/java/com/deorabanna1925/boredom/activity/InstaViewerActivity.java
@@ -16,7 +16,19 @@ public class InstaViewerActivity extends AppCompatActivity {
         binding = ActivityInstaViewerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.btnSearch.setOnClickListener(v -> {
+            String username = binding.etSearch.getText().toString();
+            if (username.isEmpty()) {
+                binding.etSearch.setError("Enter Username");
+                return;
+            }
+            binding.etSearch.setError(null);
+            getData(username);
+        });
+    }
 
+    private void getData(String username) {
+        String url = "https://www.instagram.com/" + username + "/?__a=1";
 
     }
 }
